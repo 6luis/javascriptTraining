3d4734878616c445cdeb9088ad964517f2a1d029
Luis ''tr6ll'' Gustavo <141465891+6luis@users.noreply.github.com>
2024-01-03 23:14:31 -0300

    Add files via upload

---
+++ JS Training n.3/condicionas.js
@@ -0,0 +1,9 @@
+let nota1 = Number(prompt("Digite sua primeira nota"));
+let nota2 = Number(prompt("Digite sua segunda nota"));
+let result = (nota1 + nota2) / 2;
+
+if (result >= 5) {
+  alert("boa mn mandou muito, passou de ano");
+} else {
+  alert("acontece né po reprovou mlk");
+}
